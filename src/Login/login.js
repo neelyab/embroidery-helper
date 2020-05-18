76aaf2e8b282b975f1194315d0602d00e2415af4
Amanda <neelyab@vcu.edu>
2020-05-18 12:09:39 -0400

    connected api to login and registration feature

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,14 +1,52 @@
 import React, {Component} from 'react'
 import './login.css'
+import TokenService from '../services/token-service'
+import config from '../config'
 
 class Login extends Component {
+
+    state = {error: null}
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const { username, password } = e.target;
+        const credentials = { 
+                username: username.value, 
+                password: password.value
+            }
+        fetch(`${config.API_ENDPOINT}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(credentials)
+            })
+        .then(res =>
+            (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+        )
+        .then(res=> {
+            username.value = ''
+            password.value = ''
+            TokenService.saveAuthToken(res.authToken)
+            this.props.history.push('/')
+        })
+        .catch(res => {
+            this.setState({error: res.error})
+        })
+
+
+
+    }
     render(){
         return (<div>
-            <form className="login">
+            <form className="login" onSubmit={this.handleSubmit}>
                 <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username"></input>
+                <input type="text" id="username" name="username" required></input>
                 <label htmlFor="password">Password:</label>
-                <input type="text" id="password" name="password"></input>
+                <input type="password" id="password" name="password" required></input>
+                {this.state.error && <p>{this.state.error}</p>}
                 <button type="submit">Submit</button>
             </form>
         </div>)
--- src/SignUp/signup.js
+++ src/SignUp/signup.js
@@ -1,18 +1,64 @@
 import React, {Component} from 'react'
 import './signup.css'
+import config from '../config'
 
 class SignUp extends Component {
+    state = {error: null}
+    handleSubmit = e => {
+        this.setState({error: null})
+        e.preventDefault()
+
+        const { username, password, first_name, repeat_password } = e.target
+        const REGEX = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+
+        if(password.value.length < 6 ){
+            this.setState({error: 'Password must be greater than 6 characters.'})
+        }
+        if(!REGEX.test(password)){
+            this.setState({error:'Password must contain one upper case, lower case, number and special character.'})
+        }
+        if(password.value !== repeat_password.value){
+            this.setState({error: 'Passwords do no match.'})
+        }
+        const user = {
+            username: username.value,
+            first_name: first_name.value,
+            user_password: password.value
+        }
+        fetch(`${config.API_ENDPOINT}/users`, {
+            method: 'POST',
+            headers: {
+             'content-type': 'application/json',
+            },
+            body: JSON.stringify(user),
+           })
+            .then(res =>
+              (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+                 )
+            .then(user => {
+                username.value = ''
+                password.value = ''
+                first_name.value = ''
+                repeat_password.value = ''
+                this.props.history.push('/login')
+            })
+
+    }
     render(){
         return (<div>
-            <form className="signup">
+            <form className="signup" onSubmit={this.handleSubmit}>
+                {this.state.error && <p>{this.state.error}</p>}
                 <label htmlFor="first-name">First Name:</label>
-                <input type="text" id="first-name" name="first-name"></input>
+                <input type="text" id="first-name" name="first_name" required></input>
                 <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username"></input>
-                <label htmlFor="password">Password:</label>
-                <input type="text" id="password" name="password"></input>
+                <input type="text" id="username" name="username" required></input>
+                <label htmlFor="password" onChange={this.password}>Password:</label>
+                <input type="password" id="password" name="password" required></input>
+                <p className = "password-require">Must contain <strong>one</strong> upper case, lower case, number, and special character</p>
                 <label htmlFor="repeat-password">Re-enter Password:</label>
-                <input type="text" id="repeat-password" name="repeat-password"></input>
+                <input type="password" id="repeat-password" name="repeat_password" required></input>
                 <button type="submit">Submit</button>
             </form>
         </div>)
