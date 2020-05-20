655cf55e942b739a1dce811584d2ea57a2768f75
Amanda <neelyab@vcu.edu>
2020-05-20 13:12:10 -0400

    added api endpoint url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_ENDPOINT: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://secure-woodland-74305.herokuapp.com/api',
     TOKEN_KEY: ''
 }
