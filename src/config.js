e57923337d2cf7dee7a01b496bf929fdae9e8d15
Amanda <neelyab@vcu.edu>
2020-05-20 19:13:39 -0400

    redeploying

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_ENDPOINT: 'https://secure-woodland-74305.herokuapp.com/api',
-    TOKEN_KEY: ''
+    API_ENDPOINT: `https://secure-woodland-74305.herokuapp.com/api`,
+    TOKEN_KEY: '',
 }
